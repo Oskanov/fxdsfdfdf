b2d52dae79de51eb737b26e0d3d5ba21bb7ce314
Oskanov <oskanov17@mail.ru>
2013-03-07 18:54:49 +0400

    Новый проект

--- fxdsfdfdf/fxdsfdfdf.cpp
+++ fxdsfdfdf/fxdsfdfdf.cpp
@@ -8,25 +8,15 @@ int _tmain(int argc, _TCHAR* argv[])
 {
 int n=5, i, z, t=0;
 int* m;
+printf ("Vvedite  n \n");
+scanf ("%d",&n);
 m=new int[n];
 for(i=0; i<n; i++)
 {
-m[i]=rand()%100;
+m[i]=rand()%1000;
 printf("%d ", m[i]);
 }
 printf("\n______________________________\n");
-
-
-/* for(i=0; i<(n-1); i++)
-{
-if (m[i]>m[i+1])
-{
-z=m[i];
-m[i]=m[i+1];
-m[i+1]=z;
-}
-}
-*/
 
 
 for(;;)
@@ -40,7 +30,6 @@ m[i]=m[i+1];
 m[i+1]=z;
 t++;
 }
-// else t++;
 }
 if (t==!0)
 {
